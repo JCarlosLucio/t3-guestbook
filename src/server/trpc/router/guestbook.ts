d905da0933c2d3a9741318ae280601a8ff9e7984
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-12-01 01:37:42 -0600

    Add create guestbook mutation procedure to guestbookRouter

---
+++ src/server/trpc/router/guestbook.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { publicProcedure, router } from "../trpc";
+
+export const guestbookRouter = router({
+  postMessage: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        message: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.guestbook.create({
+          data: {
+            name: input.name,
+            message: input.message,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
